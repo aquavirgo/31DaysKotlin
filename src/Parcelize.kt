30b5178ea108680ea5d85a216b51b8ac41975ce2
Jakub Guzikowski <j.guzikowski@samsung.local>
2018-05-23 14:49:07 +0200

    Day 4

---
+++ src/Parcelize.kt
@@ -0,0 +1,10 @@
+//Day 29
+// Parcelize
+
+/*@Parcelize
+data class User(val name: String, val occupation: Work) : Parcelable
+
+//build.gradle
+
+experimantal = true*/
+
---
+++ src/Scoping.kt
@@ -0,0 +1,14 @@
+//Day 31
+//Scoping
+
+val string = "a"
+
+//run
+val result = string.run { 1 }
+
+//let
+val string1 ="a"
+val result1 = string1.let { 2 }
+fun main(args: Array<String>) {
+    println(result)
+}
